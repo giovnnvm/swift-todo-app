62159dae0c057e355eae0f62cdb5ebb0c32f9ad2
Giovanni Vicentin Moratto <morattotrabalho@gmail.com>
2025-11-09 13:40:12 -0300

    chore: initialize project setup

---
+++ SwiftTODOApp/SwiftTODOApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftTODOApp
+//
+//  Created by Giovanni Vicentin Moratto on 09/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
